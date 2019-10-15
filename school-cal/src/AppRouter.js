1127f75fb4a89283e08d10e1c4ba2bb8211f4651
Thai Nguyen <discoverylight@yahoo.com>
2019-10-15 10:47:06 -0700

    fixed package-lock.json, moved login into its own dir

--- school-cal/src/AppRouter.js
+++ school-cal/src/AppRouter.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Login from "./components/Login";
 const AppRouter = () => {
   return (
     <>
       <Router>
         <Switch>
           <Route exact path="/" /> {/* For a future landing page */}
-          <Route path="/login" />
+          <Route path="/login" component={Login} />
           <Route path="/register" />
         </Switch>
       </Router>

dadbe428f4b791a44c5451dc1ebb12c7ed553618
arodri04 <augustine.rodriguez55@gmail.com>
2019-10-15 12:26:41 -0700

    fixing

--- school-cal/src/components/logincomp.js
+++ school-cal/src/components/logincomp.js
@@ -1,76 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { useState, useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-import firebase, { db } from '../firebase/index';
+import firebase, { db } from "../../firebase/index";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles(theme => ({
-  '@global': {
+  "@global": {
     body: {
-      backgroundColor: theme.palette.common.white,
-    },
+      backgroundColor: theme.palette.common.white
+    }
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
 export default function Login() {
-    
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [credentials, setCredentials] = useState({email:"", password:""});
-    const [emailError, setEmailError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
-    const login = event => {
-        event.preventDefault();
-        return firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
-        .catch(err => {console.log(err); if (err.code === "auth/invalid-email") {setEmailError(true); console.log(emailError)} if (err.code === "auth/wrong-password"){setPasswordError(true); console.log(passwordError)}})
-    }
+  const login = event => {
+    event.preventDefault();
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .catch(err => {
+        console.log(err);
+        if (err.code === "auth/invalid-email") {
+          setEmailError(true);
+          console.log(emailError);
+        }
+        if (err.code === "auth/wrong-password") {
+          setPasswordError(true);
+          console.log(passwordError);
+        }
+      });
+  };
 
-    const handleChange = event => {
-        setCredentials({...credentials, [event.target.name]: event.target.value})
-    }
-
+  const handleChange = event => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -84,7 +94,9 @@ export default function Login() {
         </Typography>
         <form className={classes.form} noValidate onSubmit={login}>
           <Grid container spacing={2}>
-            <label className={emailError ? "show": "hide"}>Email Invalid</label>
+            <label className={emailError ? "show" : "hide"}>
+              Email Invalid
+            </label>
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
@@ -94,11 +106,14 @@ export default function Login() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
-                value={credentials.email} onChange={handleChange}
+                value={credentials.email}
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12}>
-            <label className={passwordError ? "show": "hide"}>Wrong password</label>
+              <label className={passwordError ? "show" : "hide"}>
+                Wrong password
+              </label>
               <TextField
                 variant="outlined"
                 required
@@ -108,12 +123,13 @@ export default function Login() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
-                value={credentials.password} onChange={handleChange}
+                value={credentials.password}
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
-                control={<Checkbox  color="primary" />}
+                control={<Checkbox color="primary" />}
                 label="Stay signed in."
               />
             </Grid>

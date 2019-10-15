82095a5650b0cf408d0f909780f73f3bf2a7385b
ch04937 <97hernandez.c@gmail.com>
2019-10-15 15:33:25 -0700

    changing name to match components

--- school-cal/src/AppRouter.js
+++ school-cal/src/AppRouter.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import StudentRegister from "./components/StudentRegister";
 import AdminRegister from "./components/AdminRegister";
-import AdminDashboard from "./components/AdminDashboard";
-import PrivateRoute from "./routes/PrivateRoute";
+import AdminDashboard from "./components/AdminDashboard/AdminDashboard";
+// import PrivateRoute from "./routes/PrivateRoute";
 const AppRouter = () => {
   return (
     <>
@@ -14,7 +14,7 @@ const AppRouter = () => {
           <Route path="/AdminLogin" component={Login} />
           <Route path="/StudentRegister" component={StudentRegister} />
           <Route path="/AdminRegister" component={AdminRegister} />
-          <PrivateRoute path="/admin-dashboard" component={AdminDashboard} />
+          <Route path="/admin-dashboard" component={AdminDashboard} />
         </Switch>
       </Router>
     </>
--- school-cal/src/components/AdminDashboard/AdminDashboard.js
+++ school-cal/src/components/AdminDashboard/AdminDashboard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import fireapp from "../../firebase/index.js";
 
-const dummyAdminDashBoard = () => {
+const AdminDashboard = () => {
   return (
     <div>
       <h1>Admin DashBoard</h1>
     </div>
   );
 };
+export default AdminDashboard;

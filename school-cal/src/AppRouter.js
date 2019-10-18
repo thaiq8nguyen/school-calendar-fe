18f21686acb8aee2db24bd7d22f623c1d91c4a0e
Maks <maks112v@gmail.com>
2019-10-18 17:48:14 -0400

    Update AppRouter.js

--- school-cal/src/AppRouter.js
+++ school-cal/src/AppRouter.js
@@ -5,11 +5,10 @@ import AdminLogin from "./components/AdminLogin";
 import StudentRegister from "./components/StudentRegister";
 import StudentLogin from "./components/StudentLogin";
 import AdminRegister from "./components/AdminRegister";
-import StudentDashboard from './components/StudentDashboard'
+import StudentDashboard from "./components/StudentDashboard";
 import Login from "./components/Login/index";
 
 import AdminDashboard from "./components/AdminDashboard";
-import PrivateRoute from "./routes/PrivateRoute";
 
 const AppRouter = () => {
   return (
@@ -22,7 +21,7 @@ const AppRouter = () => {
           <Route path="/student-signin" component={StudentLogin} />
           <Route path="/admin-register" component={AdminRegister} />
           <Route path="/admin-dashboard" component={AdminDashboard} />
-          <Route path='/student-dashboard' component={StudentDashboard}/>
+          <Route path="/student-dashboard" component={StudentDashboard} />
         </Switch>
       </Router>
     </>

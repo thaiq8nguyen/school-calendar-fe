bb472cfb570493b584a143c4d8c8d1d100ea53bf
arodri04 <augustine.rodriguez55@gmail.com>
2019-10-22 14:41:46 -0700

    Added in a persistant drawer to pop in and out

--- school-cal/src/components/AdminDashboard/index.js
+++ school-cal/src/components/AdminDashboard/index.js
@@ -13,10 +13,55 @@ import { makeStyles } from "@material-ui/core/styles"
 import Navbar from "../../components/Navbar"
 import { AuthContext } from "../../contexts/auth/authState"
 import { app, db } from "../../firebase"
+
+
+
+import clsx from 'clsx';
+import {useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import Typography from '@material-ui/core/Typography';
+
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+
+
+
+
+
 const drawerWidth = 240
 const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex",
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
   },
   drawer: {
     width: drawerWidth,
@@ -25,36 +70,118 @@ const useStyles = makeStyles(theme => ({
   drawerPaper: {
     width: drawerWidth,
   },
-  toolbar: theme.mixins.toolbar,
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(10, 2),
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
   },
-  listItem: {
-    display: "flex",
-    justifyContent: "center",
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
-}))
+}));
 
 const AdminDashBoard = ({ history }) => {
-  const { currentUser } = useContext(AuthContext)
+  const { currentUser, signOut } = useContext(AuthContext)
   const [isAddEventOpen, setAddEvent] = useState(false)
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+
+  
+  const [userProfile, setUserProfile] = useState(null);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   useEffect(() => {
     if (!currentUser) {
       history.push("/sign-in")
     }
   }, [currentUser])
 
+  useEffect(() => {
+    const getUserProfile = async () => {
+      try {
+        const profileRef = await db.collection("users").doc(currentUser.uid);
+
+        const profile = await profileRef.get();
+        setUserProfile(profile.data());
+
+        //setUserProfile(profileRef.get());
+      } catch (error) {
+        console.log("Unable to retrieve user profile.");
+      }
+    };
+
+    getUserProfile();
+  }, []);
+
   const classes = useStyles()
   return (
     <div className={classes.root}>
-      <Navbar drawerWidth={drawerWidth} />
+        <Navbar drawerWidth={drawerWidth} />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            School Calendar
+          </Typography>
+          {userProfile && (
+            <Typography className={classes.greeting} style={{marginLeft:"80%"}}>
+              Hello, {userProfile.firstName}
+            </Typography>
+          )}
+          <Button color="inherit" onClick={signOut}>
+            Sign Out
+          </Button>
+        </Toolbar>
+      </AppBar>
       <Drawer
-        anchor="left"
-        classes={{ paper: classes.drawerPaper }}
         className={classes.drawer}
-        variant="permanent">
-        <div className={classes.toolbar} />
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
         <Divider />
         <List>
           <ListItem
@@ -64,6 +191,9 @@ const AdminDashBoard = ({ history }) => {
             Add Event
           </ListItem>
         </List>
+        <Divider />
+        
+        
       </Drawer>
       <AddEvent handleClose={() => setAddEvent(false)} open={isAddEventOpen} />
     </div>
